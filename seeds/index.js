66f51178e2a923b6e41d6ef22b4f7e2b3abe853c
DavidHE39 <dhubach1@gmail.com>
2021-01-26 09:00:00 -0500

    updating styles

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state} `,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu scelerisque felis imperdiet proin fermentum leo vel orci porta. Ipsum consequat nisl vel pretium lectus. Risus sed vulputate odio ut enim blandit volutpat maecenas. Pharetra sit amet aliquam id. Egestas purus viverra accumsan in nisl. Augue interdum velit euismod in pellentesque.',
+            price
         })
         await camp.save();
     }
